d5fb4ea4284649f28141cb206f5be1040583d2be
ishan-default <113410578+ishan-default@users.noreply.github.com>
2022-09-14 15:51:09 +0530

    Create statcounter.js
    this code is being called on before body of https://zenoti.webflow.io/landing-pages/salon-management-software-display-awareness & https://zenoti.webflow.io/landing-pages/salon-management-software-social-awareness

---
+++ statcounter.js
@@ -0,0 +1,17 @@
+<!-- Default Statcounter code for Zenoti
+https://www.zenoti.com/ -->
+<script type="text/javascript">
+var sc_project=12793637; 
+var sc_invisible=1; 
+var sc_security="d3ecb925"; 
+</script>
+<script type="text/javascript"
+src="https://www.statcounter.com/counter/counter.js"
+async></script>
+<noscript><div class="statcounter"><a title="Web Analytics
+Made Easy - Statcounter" href="https://statcounter.com/"
+target="_blank"><img class="statcounter"
+src="https://c.statcounter.com/12793637/0/d3ecb925/1/"
+alt="Web Analytics Made Easy - Statcounter"
+referrerPolicy="no-referrer-when-downgrade"></a></div></noscript>
+<!-- End of Statcounter Code -->
